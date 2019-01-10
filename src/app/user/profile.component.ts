7259cc4d64e5e485b7e55cc3fdb57e377d909726
lumpypuppy <lumpypuppy@gmail.com>
2019-01-10 11:03:24 -0500

    form validation

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,18 +1,32 @@
 import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './profile.component.html',
 })
 export class ProfileComponent implements OnInit{
     profileForm:FormGroup;
-       
+    constructor(private router:Router, private authService:AuthService){}
+
     ngOnInit(){
-         let firstName = new FormControl();
-         let lastName = new FormControl();
+         let firstName = new FormControl(this.authService.currentUser.firstName, Validators.required);
+         let lastName = new FormControl(this.authService.currentUser.lastName, Validators.required);
          this.profileForm = new FormGroup({
            firstName: firstName,
            lastName: lastName
          })
+       }
+
+       cancel(){
+         this.router.navigate(['events'])
+       }
+
+       saveProfile(formValues){
+         if(this.profileForm.valid){
+          this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
+          this.router.navigate(['events'])
+         }
        }
 }
